3121c4375d598002d91726d0a071092a75645dc6
deffsin <deffsinco@gmail.com>
2024-05-20 21:43:58 +0200

    Initial Commit

---
+++ CoinGekoWallester/CoinGekoWallesterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinGekoWallesterApp.swift
+//  CoinGekoWallester
+//
+//  Created by Denis Sinitsa on 20.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct CoinGekoWallesterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
