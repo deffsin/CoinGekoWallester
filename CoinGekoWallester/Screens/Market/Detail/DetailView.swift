b03c0a0d4e2150efd23f649ed073f8265672472b
deffsin <deffsinco@gmail.com>
2024-05-28 00:27:45 +0300

    Test commit

---
+++ CoinGekoWallester/Screens/Market/Detail/DetailView.swift
@@ -0,0 +1,16 @@
+//
+//  DetailView.swift
+//  CoinGekoWallester
+//
+//  Created by Denis Sinitsa on 28.05.2024.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        VStack {
+            Text("Test")
+        }
+    }
+}
