b8182c17445d9599ea5c36a998ffbae45b0221a1
Deniss <122041187+deffsin@users.noreply.github.com>
2024-06-01 11:48:20 +0300

    Revert "Detail Page"

--- CoinGekoWallester/Services/CoinDataService.swift
+++ CoinGekoWallester/Services/CoinDataService.swift
@@ -13,7 +13,6 @@ class CoinDataService {
     
     private init() { }
     
-    // fetch all crypto currencies
     func fetchCryptoCurrencies(currency: String, forceUpdate: Bool = false) -> AnyPublisher<[CoinModel], Error> {
         let urlWithCurrency = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=\(currency)&order=market_cap_desc&per_page=30&page=1&sparkline=true&price_change_percentage=1h,24h,7d"
         
@@ -23,18 +22,6 @@ class CoinDataService {
         
         return NetworkingManager.download(url: url, forceUpdate: forceUpdate)
             .decode(type: [CoinModel].self, decoder: JSONDecoder())
-            .eraseToAnyPublisher()
-    }
-    
-    func fetchCryptoCurrencyDetails(id: String, forceUpdate: Bool = false) -> AnyPublisher<CoinDetailModel, Error> {
-        let urlWithID = "https://api.coingecko.com/api/v3/coins/\(id)?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false"
-
-        guard let url = URL(string: urlWithID) else {
-            return Fail(error: URLError(.badURL)).eraseToAnyPublisher()
-        }
-
-        return NetworkingManager.download(url: url, forceUpdate: forceUpdate)
-            .decode(type: CoinDetailModel.self, decoder: JSONDecoder())
             .eraseToAnyPublisher()
     }
 }
--- CoinGekoWallester/Shared/Utils/ChartHelpers.swift
+++ CoinGekoWallester/Shared/Utils/ChartHelpers.swift
@@ -32,4 +32,3 @@ struct LineGraph: Shape {
         return path
     }
 }
-
